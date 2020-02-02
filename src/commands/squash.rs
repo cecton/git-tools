5030e18b57c36c807561c3b8e407684adc274b7e
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-02-02 15:32:06 +0100

    Improve safety: squash only if not behind

--- src/commands/squash.rs
+++ src/commands/squash.rs
@@ -7,16 +7,16 @@ pub fn run(params: Squash) -> Result<(), Box<dyn std::error::Error>> {
     let revision = if let Some(rev) = params.revision {
         rev
     } else if let Some(parent) = git.get_parent_branch()? {
-        let (_ahead, behind) = git.graph_ahead_behind("HEAD", parent.as_str())?;
-
-        if behind > 0 {
-            return Err(format!("The current branch is not up-to-date with {}.", parent).into());
-        }
-
         parent
     } else {
         return Err("Could not find forked point and no revision provided!".into());
     };
+
+    let (_ahead, behind) = git.graph_ahead_behind("HEAD", revision.as_str())?;
+
+    if behind > 0 {
+        return Err(format!("The current branch is not up-to-date with '{}'!", revision).into());
+    }
 
     let hash = git.reset_soft(revision.as_str(), "moving for squashing")?;
 
