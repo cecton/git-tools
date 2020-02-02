9575b39c25ed9cd9d0c3964cb61d890d4c3d828c
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-02-02 14:28:24 +0100

    Add more safety measures

--- src/commands/delete.rs
+++ src/commands/delete.rs
@@ -2,11 +2,21 @@ use crate::git::Git;
 use crate::Delete;
 
 pub fn run(params: Delete) -> Result<(), Box<dyn std::error::Error>> {
-    let repo = Git::open()?;
+    let git = Git::open()?;
 
-    repo.full_delete_branch(params.branch_name.as_str())?;
+    let branch_name = params.branch_name.as_str();
 
-    println!("Branch {} deleted.", params.branch_name);
+    if branch_name == "master" || branch_name.ends_with("/master") {
+        return Err(format!(
+            "Cannot delete '{}'!",
+            branch_name
+        )
+        .into());
+    }
+
+    git.full_delete_branch(branch_name)?;
+
+    println!("Branch {} deleted.", branch_name);
 
     Ok(())
 }
